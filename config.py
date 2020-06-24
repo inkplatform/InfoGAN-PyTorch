9171ad2e9c2c1cd747177936e0f99d997f684bae
muuvi-bit <muuvi@outlook.com>
2020-06-24 20:00:42 +0900

    first commit

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 # Dictionary storing network parameters.
 params = {
-    'batch_size': 128,# Batch size.
+    'batch_size': 64,# Batch size.
     'num_epochs': 100,# Number of epochs to train for.
     'learning_rate': 2e-4,# Learning rate.
     'beta1': 0.5,
